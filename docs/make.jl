7e4d31e17365c13a1727ab7edf6db62494233b03
Guillem <guillemborrell@gmail.com>
2020-09-20 22:53:55 +0200

    First version and formatted source

--- docs/make.jl
+++ docs/make.jl
@@ -2,20 +2,16 @@ using nng
 using Documenter
 
 makedocs(;
-    modules=[nng],
-    authors="Guillem <guillemborrell@gmail.com> and contributors",
-    repo="https://github.com/guillemborrell/nng.jl/blob/{commit}{path}#L{line}",
-    sitename="nng.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://guillemborrell.github.io/nng.jl",
-        assets=String[],
+    modules = [nng],
+    authors = "Guillem <guillemborrell@gmail.com> and contributors",
+    repo = "https://github.com/guillemborrell/nng.jl/blob/{commit}{path}#L{line}",
+    sitename = "nng.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://guillemborrell.github.io/nng.jl",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/guillemborrell/nng.jl",
-)
+deploydocs(; repo = "github.com/guillemborrell/nng.jl")
--- src/nng.jl
+++ src/nng.jl
@@ -1,5 +1,117 @@
 module nng
 
 # Write your package code here.
+const LIB = "libnng.so"
+
+mutable struct nng_socket
+    id::UInt32
+end
+
+mutable struct nng_listener
+    id::UInt32
+end
+
+mutable struct nng_dialer
+    id::UInt32
+end
+
+"""
+Low level function to get the socket ID.
+
+In many occasions, this returns -1 (Error), but the socket works anyway
+"""
+_nng_socket_id(socket::nng_socket) =
+    ccall((:nng_socket_id, LIB), Cint, (Ref{nng_socket},), Ref(socket))
+
+"""
+Low level listen
+"""
+_nng_listen(socket::nng_socket, url::String) = ccall(
+    (:nng_listen, LIB),
+    Cint,
+    (nng_socket, Cstring, Ref{nng_listener}, Cint),
+    socket,
+    url,
+    Ref(nng_listener(0)),
+    0,
+)
+"""
+Low level dial
+"""
+_nng_dial(socket::nng_socket, url::String) = ccall(
+    (:nng_dial, LIB),
+    Cint,
+    (nng_socket, Cstring, Ref{nng_dialer}, Cint),
+    socket,
+    url,
+    Ref(nng_dialer(0)),
+    0,
+)
+
+"""
+Low level PULL
+"""
+_nng_pull0_open(socket::nng_socket) =
+    ccall((:nng_pull0_open, LIB), Cint, (Ref{nng_socket},), Ref(socket))
+
+"""
+Low level PUSH
+"""
+_nng_push0_open(socket::nng_socket) =
+    ccall((:nng_push0_open, LIB), Cint, (Ref{nng_socket},), Ref(socket))
+
+"""
+Low level PUB
+"""
+_nng_pub0_open(socket::nng_socket) =
+    ccall((:nng_pub0_open, LIB), Cint, (Ref{nng_socket},), Ref(socket))
+
+"""
+Low level SUB
+"""
+_nng_sub0_open(socket::nng_socket) =
+    ccall((:nng_psub0_open, LIB), Cint, (Ref{nng_socket},), Ref(socket))
+
+"""
+Low level CLOSE
+"""
+_nng_close(socket::nng_socket) = ccall((:nng_close, LIB), Cint, (nng_socket,), socket)
+
+"""
+Low level SEND. At this point only sync operation supported
+"""
+function _nng_send(socket::nng_socket, message::AbstractString)
+    return ccall(
+        (:nng_send, LIB),
+        Cint,
+        (nng_socket, Ptr{UInt8}, Csize_t, Cint),
+        socket,
+        pointer(message),
+        sizeof(message) + 1,
+        0,
+    )
+end
+
+"""
+Low level RECV. At this point only sync operation supported
+"""
+function _nng_recv(socket::nng_socket)
+    buf = Vector{Ptr{UInt8}}(undef, 1)
+    size = Csize_t(0)
+    rval = ccall(
+        (:nng_recv, LIB),
+        Cint,
+        (nng_socket, Ptr{UInt8}, Ref{Csize_t}, Cint),
+        socket,
+        pointer(buf),
+        size,
+        1,
+    )
+
+    msg = unsafe_string(buf[1])
+    ccall((:nng_free, LIB), Cvoid, (Ptr{Cvoid}, Csize_t), buf[1], sizeof(msg))
+
+    return msg
+end
 
 end
